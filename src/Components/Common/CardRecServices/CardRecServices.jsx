a94ce75768ee0f534bdc738179c0b4cb3616e0d2
Vushil2002 <202001054@daiict.ac.in>
2024-02-24 18:24:53 +0530

    recommended cards styles update

--- src/Components/Common/CardRecServices/CardRecServices.jsx
+++ src/Components/Common/CardRecServices/CardRecServices.jsx
@@ -4,9 +4,11 @@ import VioletBtn from "../Button/VioletBtn";
 const CardRecServices = (props) => {
     return(
         <div className="RecServicesCards" style={{backgroundImage:`url(${props.bgImg})`}}>
-            <h1>{props.title}</h1>
-            <p>{props.desc}</p>
-            <VioletBtn content={"View FAQs"}/>
+            <div className="Content">
+                <h1>{props.title}</h1>
+                <p>{props.desc}</p>
+            </div>
+                <VioletBtn content={"View FAQs"}/>
         </div>
     )
 }
