33dc899eca1ad044bca076127295c58f2b44c7ce
Vushil2002 <202001054@daiict.ac.in>
2024-02-26 15:18:40 +0530

    search function added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,10 +8,10 @@ import Carousel from "./Carousel/Carousel"
 import { useState } from "react"
 
 const Header = () => {
-    const [isOpen,setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     const openMenu = () => {
-        if(isOpen) {
+        if (isOpen) {
             document.getElementById("MobileMenu").classList.add('Hidden');
             setIsOpen(false);
         } else {
@@ -20,10 +20,17 @@ const Header = () => {
         }
     }
 
+    const handleSubmit = () => {
+        const str = document.getElementById("searchInput").value;
+        console.log(str)
+        const url = `https://www.google.com/search?q=${str}`
+        window.open(url);
+    }
+
     return (
         <section className="HeaderSection">
             <div className="Header">
-                <button onClick={openMenu} className="HamBur"><img  src={hamburgerIcon} alt="hamburger" width={20} style={{margin:"0"}}/></button>
+                <button onClick={openMenu} className="HamBur"><img src={hamburgerIcon} alt="hamburger" width={20} style={{ margin: "0" }} /></button>
                 <div className="MobileMenu Hidden" id="MobileMenu">
                     <ul>
                         <li><a href="./">Home</a></li>
@@ -32,7 +39,7 @@ const Header = () => {
                         <li><a href="./">Contact Us</a></li>
                     </ul>
                 </div>
-                <img src={logo} alt="logo"/>
+                <img src={logo} alt="logo" />
                 <div className="Navlinks">
                     <ul>
                         <li><a href="./">Home</a></li>
@@ -41,9 +48,11 @@ const Header = () => {
                         <li><a href="./">Contact Us</a></li>
                     </ul>
                 </div>
-                <div className="SearchBox">
-                    <p>Search</p>
-                    <img src={searchIcon} alt="q" height={26} />
+                <div className="search">
+                    <form action="#" onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Search" name="search" id="searchInput"></input>
+                        <button onClick={handleSubmit} type="button"><img src={searchIcon} alt="Q" width={20}></img></button>
+                    </form>
                 </div>
                 {/* <input placeholder="Search"></input> */}
                 <div className="UserProfile">
